a7ef0690caf84c3c5125635a85a2f4144af01bf8
SC\annie.stolte <annie.stolte@my.simpson.edu>
2017-11-02 09:22:00 -0500

    Changed all files from .html to .php

---
+++ public_html/footer.php
@@ -0,0 +1,6 @@
+    <!-- Footer -->
+<footer>
+  <div style='text-align: center; padding-top: 50px'>
+    &copy; Contact us at please.help@localphotography.com! We love to hear from our fans!
+  </div>
+</footer>
