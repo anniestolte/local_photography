ba766510369230889b481773b0ea7e7d08250b6f
SC\annie.stolte <annie.stolte@my.simpson.edu>
2017-11-02 08:54:05 -0500

    Created footer.php file and navigation.php file. Created code to link all pages to new files.

---
+++ public_html/navigation.php
@@ -0,0 +1,31 @@
+    <!-- Navigation Bar -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+
+            <!-- Collapsed navigation -->
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          </div>
+          </div>
+            <!-- title on nav bar  -->
+            <div><a class="navbar-brand" href="#">Local Photography</a></div>
+
+      <div id="navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="index.php">Home</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="internships.php">Internships</a></li>
+          <li><a href="classes.php">Classes</a></li>
+          <li><a href="studio_history.php">Studio History</a></li>
+          <li><a href="studio_hours.php">Studio Hours</a></li>
+          <li><a href="contact.php">Contact Us</a></li>
+          <li><a href='login.php'>Log In</a></li>
+          <li><a href='signup.php'>Sign Up</a></li>
+        </ul>
+      </div>
+    </nav>
